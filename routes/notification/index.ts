40110ce97fa2b9d47f2f48c83501a763b1e55f47
arieltecay <arieltecay@gmail.com>
2025-09-13 23:22:47 -0300

    migration of routes notification from js to ts

--- routes/notification/index.ts
+++ routes/notification/index.ts
@@ -1,9 +1,8 @@
-import express from 'express';
+import express, { Router } from 'express';
 import { sendPurchaseNotification } from '../../controllers/notification/notificationSuccess.js';
 
-const router = express.Router();
+const router: Router = express.Router();
 
-// POST /api/payment/success
 router.get('/success', sendPurchaseNotification);
 
 export default router;
