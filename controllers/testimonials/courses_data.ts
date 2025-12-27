8b02b456f644c8d6ef856112b84c281c08b7ad41
arieltecay <arieltecay@gmail.com>
2025-12-27 16:13:31 -0300

    fix: Se eliminó un testimonio duplicado de la lista de testimonios.

--- controllers/testimonials/courses_data.ts
+++ controllers/testimonials/courses_data.ts
@@ -5,24 +5,19 @@ export interface Testimonial {
 }
 
 export const testimonials: Testimonial[] = [
-    {
-        id: "1",
-        name: "Vanessa",
-        description: "Gracias por explicarnos con claridad y paciencia"
-    },
-    {
-        id: "2",
-        name: "Maria",
-        description: "Excelente las clases, explicas con mucha claridad. Me encantan tus talleres."
-    },
-    {
-        id: "3",
-        name: "Paola",
-        description: "La verdad super clara para explicar, se nota que te gusta enseñar, muy cálida y predispuesta a sacarnos todas las dudas. Estaré atenta para hacer más cursos con vos."
-    },
-    {
-        id: "4",
-        name: "Paola",
-        description: "La verdad super clara para explicar, se nota que te gusta enseñar, muy cálida y predispuesta a sacarnos todas las dudas. Estaré atenta para hacer más cursos con vos."
-    }
+  {
+    id: "1",
+    name: "Vanessa",
+    description: "Gracias por explicarnos con claridad y paciencia"
+  },
+  {
+    id: "2",
+    name: "Maria",
+    description: "Excelente las clases, explicas con mucha claridad. Me encantan tus talleres."
+  },
+  {
+    id: "3",
+    name: "Paola",
+    description: "La verdad super clara para explicar, se nota que te gusta enseñar, muy cálida y predispuesta a sacarnos todas las dudas. Estaré atenta para hacer más cursos con vos."
+  }
 ];
